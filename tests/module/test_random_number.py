22f124aa001edb48ca70911ee156f45ba0d0d46a
Marylette Roa <maryletteroa@gmail.com>
2022-12-08 09:49:50 +0800

    update test random number

--- tests/module/test_random_number.py
+++ tests/module/test_random_number.py
@@ -5,9 +5,10 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..\\
 
 from module.random_number import choose_number, print_message
 
-def test_choose_number():
-    assert choose_number() in [1,2,3]
+def TestRandomNumber(object):
+    def test_choose_number(self):
+        assert choose_number() in [1,2,3]
 
-def test_print_message(capsys):
-    print_message()
-    assert capsys.readouterr().out == "Hello world!\n"
+    def test_print_message(self, capsys):
+        print_message()
+        assert capsys.readouterr().out == "Hello world!\n"
